5d79be102c7e5de9a87696515b1fb5e8e7a9d13b
Ragukumar v <ragukumar1499@gmail.com>
2024-05-04 16:44:12 +0530

    Latest

--- src/test/java/OAuth.java
+++ src/test/java/OAuth.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class OAuth {
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class OAuth {
+
+
+    @Test
+    public void get_Token(){
+
+
+        String response=given().formParam("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .formParam("client_secret","erZOWM9g3UtwNRj340YYaK_W")
+                .formParam("grant_type","client_credentials")
+                .when().log().all().post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token").asString();
+
+
+        JsonPath js=new JsonPath(response);
+        String token=js.getString("access_token");
+
+      String response2=  given().queryParam("access_token",token)
+                .when().log().all().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").asString();
+
+
+      System.out.println(response2);
+
+    }
+
+
 }
